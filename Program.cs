464995fddb8fb4fa64fc432ce0dfa49fd94209f4
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-17 18:35:03 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AddressBookSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Address Book System");
+        }
+    }
+}
+
