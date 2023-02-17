84868e77edc62d875c716c0145033100b3f8c381
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-17 19:08:42 +0530

    Edit Address Contacts

--- Program.cs
+++ Program.cs
@@ -4,20 +4,9 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to AddressBook Program ");
-
-            Console.WriteLine("Add Personal Details : firstname, lastname, email, address, city, state, pincode, phone");
-            string FirstName = Console.ReadLine();
-            string LastName = Console.ReadLine();
-            string Email = Console.ReadLine();
-            string Address = Console.ReadLine();
-            string City = Console.ReadLine();
-            string State = Console.ReadLine();
-            string Pincode = Console.ReadLine();
-            string Phone = Console.ReadLine();
-
-            AddAddress c1 = new AddAddress(FirstName, LastName, Email, Address, City, State, Pincode, Phone);
-
+            EditAddress c1 = new EditAddress();
+            String[] names = { "Shruthi", "Sunny", "Pooja", "Dharani" };
+            c1.change(names);
         }
     }
     
