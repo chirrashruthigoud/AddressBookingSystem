b9eb65c1a84f22ac75ea5badc2910299a485f344
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-22 21:27:20 +0530

    Ability to add multiple person to Address Book

--- Program.cs
+++ Program.cs
@@ -4,9 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            DeleteContacts c1 = new DeleteContacts();
-            String[] names = { "Shruthi", "Sunny", "Chinnu", "Siri" };
-            c1.change(names);
+            AddressBookMain.AddContact();
         }
     }
 }
