4582e8d3e3cea5bf8b9202fe86313f24d0c9c93d
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-06 15:50:32 +0530

    Search Person in City or in State

--- Program.cs
+++ Program.cs
@@ -8,16 +8,20 @@ namespace AddressBookSystem
         {
             List<Person> people = new List<Person>();
             people.Add(new Person { firstName = "Shruthi", lastName = "chirra", city = "jagar", state = "warangal", zipcode = "75463", country = "India", phone = "55455665", email = "shahu@gmail.com" });
+            people.Add(new Person { firstName = "Shruthi", lastName = "chirra", city = "jagar", state = "warangal", zipcode = "75463", country = "India", phone = "5545556", email = "shu@gmail.com" });
+            people.Add(new Person { firstName = "sunny", lastName = "guntuku", city = "jagar", state = "warangal", zipcode = "75463", country = "India", phone = "55455665", email = "shahu@gmail.com" });
 
-            Person newEntry1 = new Person { firstName = "Shruthi", lastName = "chirra", city = "jagar", state = "warangal", zipcode = "75463", country = "India", phone = "55455665", email = "shahu@gmail.com" };
-            if (people.Any(e => e.firstName == newEntry1.firstName))
+
+            Person newEntry1 = new Person { firstName = "Sagar", lastName = "Shahu", city = "jagar", state = "warangal", zipcode = "75463", country = "India", phone = "55455665", email = "shahu@gmail.com" };
+            if (people.Any(e => e.city == newEntry1.city || e.state == newEntry1.state))
             {
-                Console.WriteLine("Duplicate entry: {0}", newEntry1.firstName);
+                Console.WriteLine("Multiple Person in same state and same city" + " Name : " + newEntry1.firstName + ", State : " + newEntry1.state + ", City : " + newEntry1.city);
             }
             else
             {
                 people.Add(newEntry1);
             }
+
         }
     }
 }
