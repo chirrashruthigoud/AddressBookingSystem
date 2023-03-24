587d152e98585eeb83e73f0bcd12bb36bee1356e
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-24 13:51:50 +0530

    Read Or Write From Csv File

---
+++ CsvReader.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace AddressBookSystem
+{
+    internal class CsvReader
+    {
+        private StreamReader streamReader;
+        private CultureInfo culture;
+
+        public CsvReader(StreamReader streamReader, CultureInfo culture)
+        {
+            this.streamReader = streamReader;
+            this.culture = culture;
+        }
+    }
+}
