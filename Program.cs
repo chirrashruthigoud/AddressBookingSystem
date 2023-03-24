0078cfdef6a415420a2ed101b3a7abdccccdf2bf
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-24 13:08:38 +0530

    Ability To Get Contact Person

--- Program.cs
+++ Program.cs
@@ -20,13 +20,8 @@ namespace AddressBookSystem
             Person newEntry1 = new Person { firstName = "shruthi", lastName = "guntuku", city = "hnk", state = "AP", zipcode = "258741", country = "India", phone = "369852147", email = "JIhu@gmail.com" };
             if (people.Any(e => e.city == newEntry1.city || e.state == newEntry1.state))
             {
-                Console.WriteLine("Multiple Person in same state and same city" + " Name : " + newEntry1.firstName + ", State : " + newEntry1.state + ", City : " + newEntry1.city);
+                Console.WriteLine(" Contact " + newEntry1.phone);
             }
-            else
-            {
-                people.Add(newEntry1);
-            }
-
         }
     }
 }
