8ca70b27252da0aeac30d60efc62d5df87d7fa4d
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-24 12:53:46 +0530

    Ability To Check Duplicate Entry

---
+++ Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookingSystem
+{
+    public class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zipcode { get; set; }
+        public string country { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+    }
+}
